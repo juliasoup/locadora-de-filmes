60bb68d5b83ed5cc2a9e80be673bc65eff306dce
Luciano <luciano77412@gmail.com>
2025-11-05 23:21:59 -0300

    Criado repositorio locacao para adicionar a opcao de locar filmes

---
+++ src/locadora/repository/RepositorioLocacao.java
@@ -0,0 +1,21 @@
+package src.locadora.repository;
+
+import src.locadora.model.Locacao;
+import java.util.List;
+import java.util.ArrayList;
+
+public class RepositorioLocacao {
+    private List<Locacao> locacoes = new ArrayList<>();
+
+    public void adicionarLocacao(Locacao locacao) {
+        locacoes.add(locacao);
+    }
+
+    puiblic void removerLocacao(Locacao locacao) {
+        locacoes.remove(locacao);
+    }
+
+    public List<Locacao> listarLocacoes() {
+        return new ArrayList<>(locacoes);
+    }
+}
