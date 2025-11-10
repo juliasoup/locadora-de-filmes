63a22885191d961e72a628cec24d77586a42a759
Julia Soupinski Rodrigues <164052139+juliasoup@users.noreply.github.com>
2025-11-10 15:37:43 -0300

    Implement LocadoraFactory to create LocadoraService

--- locadora/service/LocadoraFactory.java
+++ locadora/service/LocadoraFactory.java
@@ -1,5 +1,17 @@
 package src.locadora.service;
+import src.locadora.repository.*;
+
 
 public class LocadoraFactory {
+
+    public static LocadoraService criarLocadoraService() {
+        return new LocadoraService(
+            RepositorioLocacao.getRepositorioLocacao(),
+            RepositorioFilme.getRepositorioFilme(),
+            RepositorioCliente.getRepositorioCliente()
+        
+        
+        );
+    }
     
 }
